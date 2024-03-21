26b71157bde370f6f57d54917f3c6a0d0a5ac87d
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-03-21 15:28:04 +0100

    migrate to storybook > 8

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,7 @@ import '../src/tailwind.css'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    // actions: { argTypesRegex: '^on.*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
