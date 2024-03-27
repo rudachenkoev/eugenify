26eab092eef676dd7fb06840007ed8c18e72fb57
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-03-27 17:17:37 +0100

    fix tailwind initialization

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,7 +3,6 @@ import '../src/tailwind.css'
 
 const preview: Preview = {
   parameters: {
-    // actions: { argTypesRegex: '^on.*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
